27c6c5c909038528c31a8d60a5072fe29d671aab
Luwkraft <by-by-ko@mail.ru>
2022-03-31 22:35:55 +0300

    Added new line

--- src/ru/mail/Main.java
+++ src/ru/mail/Main.java
@@ -1,8 +1,11 @@
 package ru.mail;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Another Line");
         System.out.println("Hello, qa.guru!");
     }
 }

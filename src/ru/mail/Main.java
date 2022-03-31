384a6bd237d380317f74c5ee1d96ef8e077319af
Luwkraft <by-by-ko@mail.ru>
2022-03-31 23:27:27 +0300

    Added new line about slow branch

--- src/ru/mail/Main.java
+++ src/ru/mail/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Another Line");
         System.out.println("Hello, qa.guru!");
+        System.out.println("slow branch");
     }
 }
